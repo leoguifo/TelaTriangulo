87c9667a1c5f8590f5c84ca812b7dd1445ce2be0
Leonardo Guilherme Fodi <32180418+leoguifo@users.noreply.github.com>
2018-09-19 15:56:13 -0300

    Implementado o codigo de testes

--- test/telatriangulo/TrianguloTest.java
+++ test/telatriangulo/TrianguloTest.java
@@ -6,121 +6,76 @@
 package telatriangulo;
 
 import java.util.Set;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author leogu
+ * @author leonardo
  */
 public class TrianguloTest {
-    
+
     public TrianguloTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    @Test
+    public void testTrianguloEquilatero() {
+        Triangulo triangulo = new Triangulo();
+
+        Set<String> resultado;
+        resultado = triangulo.make(3, 3, 3).setHipotenusa().setResultado().getResultado();
+
+        int i = 0;
+
+        for (String propriedade : resultado) {
+            if (i == 0) {
+                assertEquals("Válido", propriedade);
+            } else if (i == 1) {
+                assertEquals("Equilátero", propriedade);
+            }
+
+            i++;
+        }
     }
 
-    /**
-     * Test of make method, of class Triangulo.
-     */
     @Test
-    public void testMake_0args() {
-        System.out.println("make");
-        Triangulo instance = new Triangulo();
-        Triangulo expResult = null;
-        Triangulo result = instance.make();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testTrianguloEscaleno() {
+        Triangulo triangulo = new Triangulo();
+
+        Set<String> resultado;
+        resultado = triangulo.make(5, 7, 8).setHipotenusa().setResultado().getResultado();
+
+        int i = 0;
+
+        for (String propriedade : resultado) {
+            if (i == 0) {
+                assertEquals("Escaleno", propriedade);
+            } else if (i == 1) {
+                assertEquals("Válido", propriedade);
+            }
+
+            i++;
+        }
     }
 
-    /**
-     * Test of make method, of class Triangulo.
-     */
     @Test
-    public void testMake_3args() {
-        System.out.println("make");
-        double a = 0.0;
-        double b = 0.0;
-        double c = 0.0;
-        Triangulo instance = new Triangulo();
-        Triangulo expResult = null;
-        Triangulo result = instance.make(a, b, c);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testTrianguloIsoceles() {
+        Triangulo triangulo = new Triangulo();
+
+        Set<String> resultado;
+        resultado = triangulo.make(4, 4, 5).setHipotenusa().setResultado().getResultado();
+
+        int i = 0;
+
+        for (String propriedade : resultado) {
+            if (i == 0) {
+                assertEquals("Válido", propriedade);
+            } else if (i == 1) {
+                assertEquals("Isósceles", propriedade);
+            }
+
+            i++;
+        }
     }
 
-    /**
-     * Test of setHipotenusa method, of class Triangulo.
-     */
-    @Test
-    public void testSetHipotenusa() {
-        System.out.println("setHipotenusa");
-        Triangulo instance = new Triangulo();
-        Triangulo expResult = null;
-        Triangulo result = instance.setHipotenusa();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setResultado method, of class Triangulo.
-     */
-    @Test
-    public void testSetResultado() {
-        System.out.println("setResultado");
-        Triangulo instance = new Triangulo();
-        Triangulo expResult = null;
-        Triangulo result = instance.setResultado();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of printResultado method, of class Triangulo.
-     */
-    @Test
-    public void testPrintResultado() {
-        System.out.println("printResultado");
-        Triangulo instance = new Triangulo();
-        instance.printResultado();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getResultado method, of class Triangulo.
-     */
-    @Test
-    public void testGetResultado() {
-        System.out.println("getResultado");
-        Triangulo instance = new Triangulo();
-        Set<String> expResult = null;
-        Set<String> result = instance.getResultado();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
